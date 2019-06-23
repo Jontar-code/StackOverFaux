570bc9bf45c8b7f5f2bbd96656b9e99faec6f802
Jontar-code <51684100+Jontar-code@users.noreply.github.com>
2019-06-22 23:10:31 -0400

    Added files
    the necessary files for the StackOverFaux progarm

---
+++ Post.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace StackOverFaux
+{
+    class Post
+    {
+        public string _title;
+        public string _description;
+        private int _votes;
+        public DateTime DateCreated { get; private set; }
+
+        public Post()
+        {
+            DateCreated = DateTime.Now;
+        }
+
+        public Post(string title)
+            :this()
+        {
+            _title = title;
+        }
+
+        public Post(string title, string description)
+            :this(title)
+        {
+            _description = description;
+        }
+
+        public int GetVotes()
+        {
+            return _votes;
+        }
+
+        public void UpVote()
+        {
+            _votes +=1;
+        }
+
+        public void DownVote()
+        {
+            _votes -=1;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace StackOverFaux
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var op = new Post("","");
+
+            if (op._title == null || op._title=="")
+            {
+                string titleString;
+                Console.WriteLine("Enter a title: ");
+                titleString = Console.ReadLine();
+                op._title = titleString;
+            }
+            if(op._description==null || op._description=="")
+            {
+                string descString;
+                Console.WriteLine("Enter a Description: ");
+                descString = Console.ReadLine();
+                op._description = descString;
+            }
+            Console.Clear();
+            var thing = true;
+            while (thing)
+            {
+                
+                Console.WriteLine("Title: {0}", op._title);
+                Console.WriteLine();
+                Console.WriteLine("Description: {0}", op._description);
+                Console.WriteLine();
+                Console.WriteLine("Total Votes: {0}", op.GetVotes());
+                Console.WriteLine();
+                Console.WriteLine("To upvote, type 'up'. To downvote, type 'down'.");
+
+                Console.WriteLine();
+                Console.WriteLine("To exit, type exit.");
+
+                var input = Console.ReadLine();
+
+                if (input.ToLower() == "up")
+                {
+                    op.UpVote();
+                }
+                if (input.ToLower() == "down")
+                {
+                    op.DownVote();
+                }
+                if (input.ToLower() == "exit")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
